d383e3d2e8d432be74df957280fdd0d761e8fdb0
bradfiller64 <fillerbrad64@gmail.com>
2024-10-03 23:06:07 -0700

    Home styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import '../css/Home.css';
 import team from '../images/team.jpeg';
+import background from '../images/background.jpg';
 
-function Home() {
+const Home = () => {
     return (
-        <div>
-            <h1>Team Marv #2 Van</h1>
-            <img src={team} alt="TeamPhoto" style={{ width: 'auto', height: 'auto' }} />
-            <h2>"Marv the Marvelous" and his crew are about to embark on the mission of a lifetime!</h2>
+        <div className="home">
+            <div
+                className="background-image"
+                style={{
+                    backgroundImage: `url(${background})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    opacity: 0.5,
+                }}
+            ></div>
+            <div className="content-box">
+                <img src={team} alt="team" className="content-image" />
+                <p className="content-text">
+                    "Marv the Marvelous" and his crew of missionaries are about to embark on the adventure of a lifetime!
+                </p>
+            </div>
         </div>
     );
-}
+};
 
 export default Home;
