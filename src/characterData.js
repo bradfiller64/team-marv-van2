b06084f2e40e3cde3ab1ee12a57063fde78a4c58
bradfiller64 <fillerbrad64@gmail.com>
2024-10-05 21:55:20 -0700

    characterData.js

--- src/characterData.js
+++ src/characterData.js
@@ -1,8 +1,24 @@
+// Dynamically load all icons from the 'icons' folder
+const icons = require.context('./images/icons', false, /\.(png|jpe?g|svg)$/);
+
+// Dynamically load all photos from the 'photos' folder
+const photos = require.context('./images/photos', false, /\.(png|jpe?g|svg)$/);
+
 const characterData = [
     {
         id: 1,
         name: "Marv",
-        description: "...", //placeholder
+        description: "Description", //placeholder
+        quote: "Quote", //placeholder
+        icon: ruby - icon, //placeholder
+        photo: "path/to/marv-photo.jpg", //placeholder
+        unlockable: false, // This character is not unlockable
+    },
+    {
+        id: ,
+        name: "Marv",
+        description: "Description", //placeholder
+        quote: "Quote", //placeholder
         icon: "path/to/marv-icon.jpg", //placeholder
         photo: "path/to/marv-photo.jpg", //placeholder
         unlockable: false, // This character is not unlockable
